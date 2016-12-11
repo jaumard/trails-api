8afd36f9572b90a90a06b882dafa3890e9b67f60
scott-wyatt <scottwyatt86@gmail.com>
2016-12-11 12:58:28 -0500

    Update to Trails 2.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  Controller: require('trails-controller'),
-  Policy: require('trails-policy'),
-  Model: require('trails-model'),
-  Service: require('trails-service'),
+  Controller: require('trails/controller'),
+  Policy: require('trails/policy'),
+  Model: require('trails/model'),
+  Service: require('trails/service'),
   TrailsApp: require('trails')
 }
